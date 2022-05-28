d4f6ac39faf63369d70f3f0dc863ba5310f8eb1c
Bhushan <tucater10896601@protonmail.com>
2022-05-28 16:37:03 +0530

    about bash script comments

---
+++ script_comments.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# we should always introduce commenting scripts early in the piece. 
+# A script coment is prefaced with a # symbol . 
+#Anything after the #symbol is a comment and is not evaluated
+#by the script. 
+#the shebang, #!/bin/bash, is primarily a comment and, as such, is not 
+#evaluated by the shell. The shell running the script reads the whole
+#shebang, so it knows
+# which command interpreter to hand the script over to. 
+# a comment may be at the start of a line or a partway into the line. 
+# Shell scriptiing does not have the notion of multi-line comments
+#Welcome to bash scripting 
+# Author : Chad 
+# Date :1/5/2018
